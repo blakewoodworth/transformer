97870bb22abdbc65bc2ce60962de312e1c82473a
Blake Woodworth <blakewoodworth@gmail.com>
2024-11-13 13:20:49 -0500

    finished first ddp attempt, waiting for access to multi-gpu machine to test...

--- main_ddp.py
+++ main_ddp.py
@@ -13,34 +13,10 @@ from model import make_model
 from utils import *
 from matplotlib import pyplot as plt
 
-np.random.seed(1)
-torch.manual_seed(0)
-
-args = parse_arguments()
-vars(args)['total_steps'] = 5000
-
-setup_directories()
-prepare_tinystories()
-
-wandb.init(project='transformer-gpu', config=vars(args))
-
-if torch.cuda.is_available():
-    device = torch.device('cuda:0')
-else:
-    device = torch.device('cpu')
-
-model = make_model(args).to(device)
-print(f"# model parameters: {round(sum(p.numel() for p in model.parameters())/1e6,2)}M")
-
-
-wandb.finish()
-
-
-
 def setup(rank, world_size):
 	os.environ['MASTER_ADDR'] = 'localhost'
 	os.environ['MASTER_PORT'] = '12355'
-	dist.init_process_group('gloo', rank=rank, world_size=world_size)
+	dist.init_process_group('nccl', rank=rank, world_size=world_size)
 
 def cleanup():
 	dist.destroy_process_group()
@@ -48,7 +24,12 @@ def cleanup():
 def train(rank, world_size):
 	setup(rank, world_size)
 
+	args = parse_arguments()
+	vars(args)['total_steps'] = 50
+
 	model = make_model(args).to(rank)
+	if rank == 0:
+		print(f"# model parameters: {round(sum(p.numel() for p in model.parameters())/1e6,2)}M")
 	ddp_model = DDP(model, device_ids=[rank])
 
 	optimizer = torch.optim.AdamW(ddp_model.parameters(), lr=args.lr_max, betas=(args.beta1, args.beta2), weight_decay=args.weight_decay)
@@ -56,10 +37,13 @@ def train(rank, world_size):
 	criterion = torch.nn.CrossEntropyLoss()
 	trainloader = DataLoader(args.train_datafile, args.batch_size, args.context_length, random_seed=rank)
 
-	TQDM!!
+	if rank == 0:
+		my_tqdm = tqdm
+	else:
+		my_tqdm = lambda z: z
 
 	ddp_model.train()
-	for i in tqdm(range(1,args.total_steps+1)):
+	for i in my_tqdm(range(1,args.total_steps+1)):
 
 		X,Y = trainloader.get_batch()
 		X,Y = X.to(rank), Y.to(rank)
@@ -77,21 +61,33 @@ def train(rank, world_size):
 			grad_norm = torch.sqrt(sum([torch.norm(p.grad)**2 for p in ddp_model.parameters()])).item()
 			wandb.log({'loss': loss.item(), 'param_norm': param_norm, 'grad_norm': grad_norm, 'stepsize': optimizer.param_groups[0]['lr']})
 
-			# if i % args.eval_frequency == 0 or i == args.total_steps:
-			# 	perplexity = calculate_perplexity(model, args)
-			# 	wandb.log({'test_perplexity': perplexity}, commit=False)
-			
-			# if i % args.checkpoint_frequency == 0 or i == args.total_steps:
-			# 	outfile = args.checkpoint_path + f'checkpoint{i}.pt'
-			# 	save_checkpoint(model, outfile)
-
 		optimizer.step()
 		scheduler.step()
 
+	if rank == 0:
+		outfile = args.checkpoint_path + f'checkpoint.pt'
+		save_checkpoint(ddp_model, outfile)
+	cleanup()
 
+def run_parallel(train_fn, world_size):
+	mp.spawn(train_fn, args=(world_size,), nprocs=world_size, join=True)
 
-# if __name__ == '__main__':
-# 	pass
+if __name__ == '__main__':
+
+	np.random.seed(1)
+	torch.manual_seed(0)
+
+	setup_directories()
+	prepare_tinystories()
+
+	wandb.init(project='transformer-gpu', config=vars(args))
+
+	n_gpus = torch.cuda.device_count()
+	assert n_gpus > 1, 'Requires > 1 GPUs available'
+	run_parallel(train, world_size)
+
+	wandb.finish()
+
 
 
 
